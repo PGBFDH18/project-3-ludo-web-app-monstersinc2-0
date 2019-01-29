1283cb5373301d451e586dd0b0a4a2f515ead5db
Joachim Nordeke <joachim@nordeke.se>
2019-01-29 16:31:25 +0100

    Updated Stats-page with testobject Winner

---
+++ src/WebApp/Models/Winner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Winner
+    {
+        public int GameID { get; set; }
+        public string User { get; set; }
+        public int Points { get; set; }
+    }
+}

984b599bc0b998c83847ce21dda3f14d42f0cedd
Joachim Nordeke <joachim@nordeke.se>
2019-02-05 15:11:54 +0100

    Game.cshtml and site.js changed

--- src/WebApp/wwwroot/js/site.js
+++ src/WebApp/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function movePiece(brick, target) {
+    document.getElementById(target).appendChild(
+        document.getElementById(brick));
+}

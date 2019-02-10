1f5a9b2920d230be85debad86228f1840641695d
Giss47 <ghassan_aidi@hotmail.com>
2019-02-10 11:10:33 +0100

    Testing Azure CD

--- src/WebApp/Controllers/LudoController.cs
+++ src/WebApp/Controllers/LudoController.cs
@@ -8,20 +8,32 @@ namespace WebApp.Controllers
     public class LudoController : Controller
     {
 
-        private ILudoGameAPIProccessor _ludoProccessor { get; }
-        private IPlayerFormExtractor _extractor { get; }
         public LudoController(ILudoGameAPIProccessor ludoProccessor, IPlayerFormExtractor extractor)
         {
             _ludoProccessor = ludoProccessor;
             _extractor = extractor;
         }
 
+        private ILudoGameAPIProccessor _ludoProccessor { get; }
+        private IPlayerFormExtractor _extractor { get; }
 
+        /// <summary>
+        /// Retruns an unvalidated new empty form to fill by user
+        /// </summary>
+        /// <returns>Index view</returns>
         public IActionResult NewForm()
         {
             return View("Index");
         }
 
+        /// <summary>
+        /// validates entry retruned by form then sends 
+        /// request to the LudoAPI with info to start an ew game   
+        /// </summary>
+        /// <param name="players"></param>
+        /// <returns>Eitehr return an index view if 
+        /// validation fails along with error messages
+        /// or redirect to Game view</returns>
         public IActionResult Index(PlayerBindingModel players)
         {
             if (!ModelState.IsValid)

13976559cc01bceb3e9930d7f1aef4244a11975c
Giss47 <ghassan_aidi@hotmail.com>
2019-02-13 13:30:19 +0100

    Littel more refactoring to Validation attributes

--- src/WebApp/Models/BindingModel/PlayerBindingModel.cs
+++ src/WebApp/Models/BindingModel/PlayerBindingModel.cs
@@ -8,7 +8,7 @@ namespace WebApp.Models.BindingModel
 
         #region
         [PlayerCustom]
-        [RegularExpression(@"[A-Za-z0-9 ]*", ErrorMessage = "Only letters and numbers")]
+        [RegularExpression(@"[\w ]*", ErrorMessage = "Only letters and numbers")]
         [StringLength(30, ErrorMessage = "Maximum length is {1}")]
         [MinLength(2, ErrorMessage = "At least 2 Charachter")]
         public string Player1Name { get; set; }
@@ -19,7 +19,7 @@ namespace WebApp.Models.BindingModel
 
         #region
         [PlayerCustom]
-        [RegularExpression(@"[A-Za-z0-9 ]*", ErrorMessage = "Only letters and numbers")]
+        [RegularExpression(@"[\w ]*", ErrorMessage = "Only letters and numbers")]
         [StringLength(45, ErrorMessage = "Maximum length is {1}")]
         [MinLength(2, ErrorMessage = "At least 2 Charachter")]
         public string Player2Name { get; set; }
@@ -30,7 +30,7 @@ namespace WebApp.Models.BindingModel
         #endregion // Player2m
 
         #region
-        [RegularExpression(@"[A-Za-z0-9 ]*", ErrorMessage = "Only letters and numbers")]
+        [RegularExpression(@"[\w ]*", ErrorMessage = "Only letters and numbers")]
         [StringLength(45, ErrorMessage = "Maximum length is {1}")]
         [MinLength(2, ErrorMessage = "At least 2 Charachter")]
         public string Player3Name { get; set; }
@@ -42,7 +42,7 @@ namespace WebApp.Models.BindingModel
         #endregion // Player3
 
         #region
-        [RegularExpression(@"[A-Za-z0-9 ]*", ErrorMessage = "Only letters and numbers")]
+        [RegularExpression(@"[\w ]*", ErrorMessage = "Only letters and numbers")]
         [StringLength(45, ErrorMessage = "Maximum length is {1}")]
         [MinLength(2, ErrorMessage = "At least 2 Charachter")]
         public string Player4Name { get; set; }

0552584fe87a9954b38ccab5801ba4da977e44be
Joachim Nordeke <joachim@nordeke.se>
2019-02-21 08:47:48 +0100

    Update Startup.cs

--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -54,12 +54,9 @@ namespace WebApp
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
                 .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
                 .AddDataAnnotationsLocalization();
-
-
-
+            
             services.AddScoped<ILudoGameAPIProccessor, LudoGameAPIProccessor>();
             services.AddTransient<IPlayerFormExtractor, PlayerFormExtractor>();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
